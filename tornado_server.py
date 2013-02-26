8be933cbad16e11c03fa2d1769e4da6222850323
Livien Yin <livienyin@Liviens-MacBook-Air.local>
2013-02-25 22:18:11 -0800

    First.

---
+++ tornado_server.py
@@ -0,0 +1,36 @@
+import argparse
+import tornado.httpserver
+from tornado import web
+
+def parse_host_and_port():
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+                '--port',
+                action='store',
+                type=int,
+                dest='port',
+                default=5000,
+                help='The port that should be listned on.',
+        )
+        parser.add_argument(
+                '--host',
+                action='store',
+                type=str,
+                dest='host',
+                default='0.0.0.0',
+                help='The IP address on which to run the server.'
+        )
+        namespace = parser.parse_args()
+        return namespace.host, namespace.port
+
+application = web.Application(
+    [
+        (r"/(.*)", web.StaticFileHandler, {"path": "./static"}),
+    ]
+)
+
+if __name__ == '__main__':
+    host, port = parse_host_and_port()
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(port, address=host)
+    tornado.ioloop.IOLoop.instance().start()
